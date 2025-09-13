4e66874a93baad9042abf87a44e88328d9af44cf
melonM <15691549+melonm@user.noreply.gitee.com>
2025-09-14 01:37:01 +0800

    feat(Notification组件): 定义通知组件定义通知主题。在每种布局中都应用该组件定义消息

---
+++ src/layouts/components/Notification.vue
@@ -0,0 +1,53 @@
+<!--------------------------------
+ - 订阅消息通知
+ - Author    ZhongYao.Huang (https://github.com/HuangZhongYao)
+ - Time      2025-09-14 0:41
+ - Copyright © 2024 ZuuuuYao By Github
+ --------------------------------->
+<script setup>
+import { useNotification } from 'naive-ui'
+import { topic, webSocketService } from '@/utils/websocket/index'
+
+const notification = useNotification()
+
+watch(() => webSocketService.connected.value, () => {
+  // 订阅广播通知消息
+  webSocketService.subscribe(topic.testNotificationTopic, (message) => {
+    notification.create({
+      title: '收到一条全体通知',
+      content: `${message}`,
+      duration: 10000,
+      closable: true,
+      onAfterEnter: () => {
+
+      },
+      onAfterLeave: () => {
+
+      },
+    })
+  })
+  // 订阅个人通知消息
+  webSocketService.subscribe(topic.personalTopic, (message) => {
+    notification.create({
+      title: '收到一条通知',
+      content: `${message}`,
+      duration: 10000,
+      closable: true,
+      onAfterEnter: () => {
+
+      },
+      onAfterLeave: () => {
+
+      },
+    })
+  })
+})
+</script>
+
+<template>
+  <div class="" />
+</template>
+
+<style scoped>
+
+</style>
